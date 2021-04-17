51aaf8040435a60083e46da3250d0ba243573540
Seppo Pakonen <seppo.pakonen@gmail.com>
2021-04-17 15:42:32 +0300

    .

---
+++ uppsrc/CodeGen/CodeGen.h
@@ -0,0 +1,10 @@
+#ifndef _CodeGen_CodeGen_h_
+#define _CodeGen_CodeGen_h_
+
+#include <CtrlLib/CtrlLib.h>
+using namespace Upp;
+
+#include "Editor.h"
+
+
+#endif
---
+++ uppsrc/CodeGen/Editor.h
@@ -0,0 +1,16 @@
+#ifndef _CodeGen_Editor_h_
+#define _CodeGen_Editor_h_
+
+
+
+class CodeGenApp : public TopWindow {
+	
+	
+public:
+	typedef CodeGenApp CLASSNAME;
+	CodeGenApp();
+	
+	
+};
+
+#endif
