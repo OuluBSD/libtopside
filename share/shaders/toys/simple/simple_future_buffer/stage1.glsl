e22d79e8aa80403c1b169d31736c8ddfa9ded511
Seppo Pakonen <seppo.pakonen@gmail.com>
2022-05-05 18:06:05 +0300

    AtomShellProto: test 05f passes

--- share/shaders/toys/simple/simple_future_buffer/stage1.glsl
+++ share/shaders/toys/simple/simple_future_buffer/stage1.glsl
@@ -1,6 +1,12 @@
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    vec2 uv = fragCoord.xy / iResolution.xy;   
+    vec2 coord = fragCoord;
+    vec2 change;
+    float t = iTime * 6;
+    change.x = sin(t);
+    change.y = cos(t);
+    coord = coord + change * cos(iTime * 0.2);
+    vec2 uv = coord / iResolution.xy;   
     fragColor = texture(iChannel0,uv);
 }
--- share/shaders/toys/simple/simple_future_buffer/stage2.glsl
+++ share/shaders/toys/simple/simple_future_buffer/stage2.glsl
@@ -1,6 +1,9 @@
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    vec2 uv = fragCoord.xy / iResolution.xy;   
-    fragColor = texture(iChannel0,uv);
+    vec2 uv = fragCoord.xy / iResolution.xy;
+    vec4 orig = texture(iChannel0,uv);
+    vec4 modified = texture(iChannel1,uv);
+    float mixf = 0.005;
+    fragColor = orig * mixf + modified * (1.0 - mixf);
 }
