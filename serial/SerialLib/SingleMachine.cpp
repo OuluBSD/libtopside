62e248fec5016cb400bfd8f695763ad6551d16aa
Seppo Pakonen <seppo.pakonen@gmail.com>
2022-09-01 23:29:32 +0300

    SerialLib: revert SingleMachine

---
+++ serial/SerialLib/SingleMachine.cpp
@@ -0,0 +1,45 @@
+#include "SerialLib.h"
+
+NAMESPACE_TOPSIDE_BEGIN
+
+
+bool SingleMachine::Open(void(*arg_fn)()) {
+	using namespace Parallel;
+	using namespace Serial;
+	
+	const AppFlags& flags = GetAppFlags();
+	Machine& mach = GetActiveMachine();
+	
+	RegistrySystemRef reg = mach.Add<RegistrySystem>();
+    mach.Add<AtomSystem>();
+	mach.Add<LinkSystem>();
+	
+	mach.Add<AtomStore>();
+	mach.Add<LinkStore>();
+	
+	mach.Add<SpaceStore>();
+	mach.Add<LoopStore>();
+	
+    TODO //mach.Add<ScriptLoader>();
+    
+	
+    reg->SetAppName("Non-screen machine");
+    
+    if (arg_fn)
+        arg_fn();
+    
+    if (!mach.Start())
+		return false;
+    
+	return true;
+}
+
+void SingleMachine::Close() {
+	using namespace Parallel;
+	
+	Machine& mach = GetActiveMachine();
+	mach.Stop();
+}
+
+
+NAMESPACE_TOPSIDE_END
